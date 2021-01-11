7848a3d9400828659ab2b356d185451eaadcff0b
maxwellhowell8 <72665159+maxwellhowell8@users.noreply.github.com>
2021-01-11 12:28:07 -0500

    Update Stocks.vb

--- Stocks.vb
+++ Stocks.vb
@@ -1,107 +1,87 @@
 Sub stocks()
-Dim ticker As String
-Dim open_value, close_value, stock_volume, percent_change, diff  As Double
-Dim start, j As Integer
-
-
-
-
-Dim ws As Worksheet
-
-
-'Loop through all stocks for one year
+Dim total As Double
+Dim i As Long
+Dim change As Double
+Dim j As Integer
+Dim start As Long
+Dim rowCount As Long
+Dim percentChange As Double
+Dim days As Integer
+Dim dailyChange As Double
+Dim averageChange As Double
+j = 0
+total = 0
+change = 0
+start = 2
 For Each ws In Worksheets
-
-
 'Create the column headings
 ws.Range("I1").Value = "Ticker"
 ws.Range("J1").Value = "Yearly Change"
 ws.Range("K1").Value = "Percent Change"
 ws.Range("L1").Value = "Total Stock Volume"
-
 ws.Range("P1").Value = "Ticker"
 ws.Range("Q1").Value = "Value"
 ws.Range("O2").Value = "Greatest % Increase"
 ws.Range("O3").Value = "Greatest % Decrease"
 ws.Range("O4").Value = "Greatest Total Volume"
-
 'Defining
-start = 2
-volume = 0
-j = 2
-
-
 Last_row = ws.Cells(Rows.Count, 1).End(xlUp).Row
 For i = 2 To Last_row
-
 'sum of volume
-If ws.Cells(i, 1).Value <> ws.Cells(i + 1, 1).Value Then
+   If ws.Cells(i, 1).Value <> ws.Cells(i + 1, 1).Value Then
+       volume = volume + ws.Cells(i, 7).Value
+       ticker = ws.Cells(i, 1).Value
+       If total = 0 Then
+               ' print the results
+               Range("I" & 2 + j).Value = Cells(i, 1).Value
+               Range("J" & 2 + j).Value = 0
+               Range("K" & 2 + j).Value = "%" & 0
+               Range("L" & 2 + j).Value = 0
+           Else
+               ' Find First non zero starting value
+               If Cells(start, 3) = 0 Then
+                   For find_value = start To i
+                       If Cells(find_value, 3).Value <> 0 Then
+                           start = find_value
+                           Exit For
+                       End If
+                    Next find_value
+               End If
+               ' Calculate Change
+               change = (Cells(i, 6) - Cells(start, 3))
+               percentChange = Round((change / Cells(start, 3) * 100), 2)
+               ' start of the next stock ticker
+               start = i + 1
+               ' print the results
+               Range("I" & 2 + j).Value = Cells(i, 1).Value
+               Range("J" & 2 + j).Value = Round(change, 2)
+               Range("K" & 2 + j).Value = "%" & percentChange
+               Range("L" & 2 + j).Value = total
+               ' colors positives green and negatives red
+               Select Case change
+                   Case Is > 0
+                       Range("J" & 2 + j).Interior.ColorIndex = 4
+                   Case Is < 0
+                       Range("J" & 2 + j).Interior.ColorIndex = 3
+                   Case Else
+                       Range("J" & 2 + j).Interior.ColorIndex = 0
+               End Select
+           End If
+           ' reset variables for new stock ticker
+           total = 0
+           change = 0
+           j = j + 1
+           days = 0
+       ' If ticker is still the same add results
+       Else
+           total = total + Cells(i, 7).Value
+       End If
+   Next i
+Next ws
 
-volume = volume + ws.Cells(i, 7).Value
-ticker = ws.Cells(i, 1).Value
-ws.Cells(j, 9).Value = ticker
-ws.Cells(j, 12).Value = volume
+End Sub
 
-' Yearly Change
-
-open_value = ws.Cells(start, 3).Value
-close_value = ws.Cells(i, 6).Value
-
-'percent change
-
-diff= close_value - open_value
-ws.Range("J" & j).Value = diff
-ws.Range("K" & j).Value = (diff / open_value) * 100
-
-
-
-
-
-
-start = i + 1
-volume = 0
-j = j + 1
-
-
-Else
-
-volume = volume + ws.Cells(i, 7).Value
-
-
-End If
-
-
-    Dim rg As Range
-    
-        Case Is >= 0
-            With color_cell
-                .Interior.Color = vbGreen
-            End With
-        Case Is < 0
-            With color_cell
-                .Interior.Color = vbRed
        
-    Next g
-
-
-
-
-Next i
-
-Next ws
-
-
-End Sub
-
-
-
-Next i
-
-Next ws
-
-
-End Sub
-
 
 
 
